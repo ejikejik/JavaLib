9082876b3cdb9845de47dbe8f220c7d007e78cc6
ejikejik <ivbaturin@mail.ru>
2013-08-13 11:02:54 +0400

    Update IEventDispatcher.java
    added hasEventListener

--- ejik/util/events/IEventDispatcher.java
+++ ejik/util/events/IEventDispatcher.java
@@ -5,5 +5,6 @@ public interface IEventDispatcher {
 	public void addEventListener(String eventType, IEventListener listener);
 	public void removeEventListener(String eventType, IEventListener listener);
 	public void dispatchEvent(Event event);
+	public boolean hasEventListener(String eventType);
 
 }
